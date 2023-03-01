e66896a48cbc2a59525a8ea0e4f29ee043d24010
Md. Ariful ISlam <arifewucse2016@gmail.com>
2023-03-01 10:30:54 +0600

    initial

---
+++ app/src/main/java/com/cit/k_pagination3/data/model/Result.kt
@@ -0,0 +1,15 @@
+package com.cit.k_pagination3.data.model
+
+import androidx.room.Entity
+
+@Entity()
+data class Result(
+    val _id: String,
+    val author: String,
+    val authorSlug: String,
+    val content: String,
+    val dateAdded: String,
+    val dateModified: String,
+    val length: Int,
+    val tags: List<String>
+)
